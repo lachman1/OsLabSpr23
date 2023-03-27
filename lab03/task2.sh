66129fadbf1e26fde7fa6293591d9d84eb2fabc1
lachman das <123716499+lachman1@users.noreply.github.com>
2023-03-27 14:38:39 +0500

    Update task2.sh

--- lab03/task2.sh
+++ lab03/task2.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # Get network information
 network_info=$(ifconfig)
-
 # Define email details
 to_address="lachmand26@gmail.com"
-subject="Network information"
-body="Here's the network information for the system:\n\n$network_info"
+subject="information about network"
+body="the network information for the system:\n\n$network_info"
 
 # Send email
 echo -e "$body" | mailx -s "$subject" "$to_address"
